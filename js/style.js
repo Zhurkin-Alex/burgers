7c1dfdb8e60efa8acb4159f8db9378d1921f0721
zhurkin aLEX <zhurkin aLEX>
2019-12-13 00:58:29 +0300

    ошибка в слайдере

--- js/style.js
+++ js/style.js
@@ -22,10 +22,29 @@ close.addEventListener('click', handrer);
 //   var header = document.querySelector('.header');
 //   header.classList.remove('header_fullcreen');
 // });
-var composition = document.querySelector('.composition');
-var close = document.querySelector('.composition__close');
-close.addEventListener('click', function(e){
- composition.style.display="none";
- 
 
-});
+
+// Крестик для слайдера
+// var composition = document.querySelector('.composition');
+// var close = document.querySelector('.composition__close');
+// close.addEventListener('click', function(e){
+//  composition.style.display="none";
+ // });
+
+ function Slider (selector) {
+   var slider = document.querySelector(selector);
+   var list = slider.querySelector('ul');
+   var countslides = list.children.length;
+   var activeSlide = 0;
+   
+   slider.addEventListener('click', function(e){
+    var target = e.target.closest('.scroll-btn');
+    console.log(target)
+
+    if (target.classlist.contains('scroll-btn') ){
+     console.log(target)
+    }
+   })
+ }
+ var slider1 = new Slider('#slider1');
+console.log(slider1);
