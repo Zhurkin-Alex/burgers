ba38d775e3abe0d375dc565b93d245989c227172
zhurkin aLEX <zhurkin aLEX>
2019-12-14 15:18:08 +0300

    3 неделя с багами слайда

--- js/style.js
+++ js/style.js
@@ -31,20 +31,96 @@ close.addEventListener('click', handrer);
 //  composition.style.display="none";
  // });
 
- function Slider (selector) {
+ function Slider (selector, settings) {
    var slider = document.querySelector(selector);
    var list = slider.querySelector('ul');
    var countslides = list.children.length;
    var activeSlide = 0;
    
-   slider.addEventListener('click', function(e){
-    var target = e.target.closest('.scroll-btn');
-    console.log(target)
+   list.style.transition = `${settings.duration || .5}s ${settings.animate || 'ease-in'}`;
 
-    if (target.classlist.contains('scroll-btn') ){
-     console.log(target)
+
+   slider.addEventListener('click', (e) =>{
+    var target = e.target.closest('.scroll-btn');
+    
+
+    if (target && target.classList.contains('scroll-btn') ){
+      e.preventDefault();
+      var vector = target.dataset.vector;
+     
+      this[vector]();
     }
-   })
+   });
+
+   this.previous = function(){
+     if (activeSlide > 0) {
+      activeSlide--;
+     }else if (settings.loop){
+      activeSlide = countslides;
+    }
+    
+    translateX();
+  }
+  
+  this.next = function () {
+    if (activeSlide<countslides -1){
+      activeSlide++;
+    } else if (settings.loop){
+      activeSlide = 0;
+    }
+    
+    translateX();
+  }
+
+ function translateX(){
+    list.style.transform = `translate(${-(activeSlide * 100)}%)`;
+  }
  }
- var slider1 = new Slider('#slider1');
-console.log(slider1);
+
+ var slider1 = new Slider('#slider1', {
+   loop: true,
+   animate: 'cubic-bezier(0,.06,.99,.38)',
+   duration: .4
+ });
+
+//   setInterval(function(){
+//    slider1.next();
+//  },5000);
+// console.log(slider1);
+
+window.addEventListener('keydown', (e)=> {
+if ( e.keyCode === 39){
+  slider1.next();
+}
+if ( e.keyCode === 37){
+  slider1.previous();
+}
+})
+
+
+
+
+function Accordion (selector) {
+var acco = document.querySelector(selector);
+var items = acco.children;
+acco.addEventListener('click', function (e){
+  var target = e.target.closest('.menu__trigger');
+
+  if(target) {
+   e.preventDefault();
+
+   if (target.parentNode.classList.contains('active')){
+    return target.parentNode.classList.remove('active');
+   }
+  
+   for (let index = 0; index < items.length; index++)     {
+     const element = items[index];
+
+     element.classList.remove('active');
+
+     target.parentNode.classList.add('active');
+   }
+  }
+})
+}
+var aco = new Accordion ('#acco');
