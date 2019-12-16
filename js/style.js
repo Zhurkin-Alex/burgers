a514c38ddfc7de2684cf628b264321e7b40d48c6
zhurkin aLEX <zhurkin aLEX>
2019-12-16 22:51:23 +0300

    баги слайда

--- js/style.js
+++ js/style.js
@@ -61,7 +61,7 @@ close.addEventListener('click', handrer);
      if (activeSlide > 0) {
       activeSlide--;
      }else if (settings.loop){
-      activeSlide = countslides;
+      activeSlide = countslides -1;
     }
     
     translateX();
@@ -191,3 +191,41 @@ sendButton.addEventListener('click', function(event){
   field.nextElementSibling.textContent = field.validationMessage;
   return field.checkValidity();
  }
+
+
+
+
+//  ---------------оверлей------------------
+// var revievs__btn = document.querySelector('.revievs__btn');
+// var close = document.querySelector('.overlay__close');
+// var header = document.querySelector('.revievs__hover');
+// var handrer = function(e) {
+//   header.classList.toggle('header_fullcreen');
+// };
+
+
+// revievs__btn.addEventListener('click', handrer);
+
+// close.addEventListener('click', handrer);
+
+
+// var humburger = document.querySelector('.burger__menu')
+// var close = document.querySelector('.header__close');
+
+// humburger.addEventListener('click', function(e){
+//   var header = document.querySelector('.header');
+//   header.classList.add('header_fullcreen');
+// });
+// close.addEventListener('click', function(e){
+//   var header = document.querySelector('.header');
+//   header.classList.remove('header_fullcreen');
+// });
+
+
+// Крестик для слайдера
+// var composition = document.querySelector('.composition');
+// var close = document.querySelector('.composition__close');
+// close.addEventListener('click', function(e){
+//  composition.style.display="none";
+ // });
+
