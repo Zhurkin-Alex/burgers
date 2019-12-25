0e66d166d224a10d45c811534d55c09ca5f5d3ad
zhurkin aLEX <zhurkin aLEX>
2019-12-25 08:19:17 +0300

    финал

--- js/style.js
+++ js/style.js
@@ -202,38 +202,39 @@ im.mask(selector);
 
 
 // -------popap-------
-function commentsPopup () {
-  const openPopup = document.querySelectorAll('.js-feedback-modal');
-  const popup = document.querySelector('.overlay');
-  const closePopup = popup.querySelector('.js-close');
-  const popupTitle = popup.querySelector('.overlay__title');
-  const popupText = popup.querySelector('.overlay__text');
+// function commentsPopup () {
+//   const openPopup = document.querySelectorAll('.js-feedback-modal');
+//   const popup = document.querySelector('.overlay');
+//   const closePopup = popup.querySelector('.js-close');
+//   const popupTitle = popup.querySelector('.overlay__title');
+//   const popupText = popup.querySelector('.overlay__text');
   
 
-  for (let i=0; i < openPopup.length; i++) {
-    openPopup[i].addEventListener('click', function() {
+//   for (let i=0; i < openPopup.length; i++) {
+//     openPopup[i].addEventListener('click', function() {
       
-        popup.style.display = 'inline-block'
-        let thisParent = this.parentNode;
-        let name = thisParent.querySelector('.revievs__name').innerText;
-        let content = thisParent.querySelector('.revievs__text p').innerText;
-
-        popupTitle.innerText = name;
-        popupText.innerText = content;
-    })
-}
-closePopup.addEventListener('click', function(e) {
+//         popup.style.display = 'inline-block';
+//         let thisParent = this.parentNode;
+//         let name = thisParent.querySelector('.revievs__name').innerText;
+//         let content = thisParent.querySelector('.revievs__text p').innerText;
+        
+//         popupTitle.innerText = name;
+//         popupText.innerText = content;
+//     })
+//     console.log(thisParent);
+// }
+// closePopup.addEventListener('click', function(e) {
     
-  event.preventDefault();
-  popup.style.display = 'none';
-})
-popup.addEventListener('click', function(e) {
-if(e.target.classList.contains('popup')) {
-    closePopup.click();
-}
-})
-}
-commentsPopup();
+//   event.preventDefault();
+//   popup.style.display = 'none';
+// })
+// popup.addEventListener('click', function(e) {
+// if(e.target.classList.contains('popup')) {
+//     closePopup.click();
+// }
+// })
+// }
+// commentsPopup();
 
 // ------меню, вертикальный слайдер, открытие и закрытие---------
 function Accordion (selector) {
@@ -452,15 +453,124 @@ if(isMobile){
 
 
 
+// let player;
+// function onYouTubeIframeAPIReady() {
+//   player = new YT.Player('yt-player', {
+//     height: '360',
+//     width: '640',
+//     videoId: 'M7lc1UVf-VE',
+//     events: {
+//       // 'onReady': onPlayerReady,
+//       // 'onStateChange': onPlayerStateChange
+//     },
+//     playerVars: {
+//       controls: 0,
+//       disablekb: 0,
+//       showinfo: 0,
+//       rel: 0,
+//       autoplay: 0,
+//       modestbranding: 0
+//     }
+//   });
+// }
+// $('.player__start').on("click", e => {
+//   player.playVideo();
+// })
+
 let player;
+
+const formatTime = timeSec => {
+  const roundTime = Math.round(timeSec);
+
+  const minutes = Math.floor(roundTime / 60);
+  const seconds = roundTime - minutes * 60;
+
+  const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  return `${minutes}:${formattedSeconds}`;
+};
+
+const onPlayerReady = () => {
+  let interval;
+  let durationSec = player.getDuration();
+
+  $(".player__duration-estimate").text(formatTime(durationSec));
+
+  if (typeof interval !== "undefined") {
+    clearInterval(interval);
+  }
+
+  interval = setInterval(() => {
+    const completedSec = player.getCurrentTime();
+    const completedPercent = (completedSec / durationSec) * 100;
+
+    $(".player__playback-button").css({
+      left: `${completedPercent}%`
+    });
+
+    $(".player__duration-completed").text(formatTime(completedSec));
+  }, 1000);
+};
+
+const eventsInit = () => {
+  $(".player__start").on("click", e => {
+    e.preventDefault();
+    const btn = $(e.currentTarget);
+
+    if (btn.hasClass("paused")) {
+      player.pauseVideo();
+    } else {
+      player.playVideo();
+    }
+  });
+
+  $(".player__playback").on("click", e => {
+    const bar = $(e.currentTarget);
+    const newButtonPosition = e.pageX - bar.offset().left;
+    const buttonPosPercent = (newButtonPosition / bar.width()) * 100;
+    const newPlayerTimeSec = (player.getDuration() / 100) * buttonPosPercent;
+
+    $(".player__playback-button").css({
+      left: `${buttonPosPercent}%`
+    });
+
+    player.seekTo(newPlayerTimeSec);
+  });
+
+  $(".player__splash").on("click", e => {
+    player.playVideo();
+  });
+};
+
+const onPlayerStateChange = event => {
+  const playerButton = $(".player__start");
+  /*
+  -1 (воспроизведение видео не начато)
+  0 (воспроизведение видео завершено)
+  1 (воспроизведение)
+  2 (пауза)
+  3 (буферизация)
+  5 (видео подают реплики).
+   */
+  switch (event.data) {
+    case 1: 
+      $('.player__wrapper').addClass('active-video');
+      playerButton.addClass("paused");
+      break;
+    case 2: 
+      playerButton.removeClass("paused");
+      break;
+  }
+};
+
 function onYouTubeIframeAPIReady() {
-  player = new YT.Player('yt-player', {
-    height: '360',
-    width: '640',
-    videoId: 'M7lc1UVf-VE',
+  player = new YT.Player("yt-player", {
+    height: "405",
+    width: "660",
+    videoId: "zmg_jOwa9Fc",
     events: {
-      // 'onReady': onPlayerReady,
-      // 'onStateChange': onPlayerStateChange
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange
     },
     playerVars: {
       controls: 0,
@@ -472,6 +582,5 @@ function onYouTubeIframeAPIReady() {
     }
   });
 }
-$('.player__start').on("click", e => {
-  player.playVideo();
-})
+
+eventsInit();
